79fe833b9a7e13b7f313cefc872afd77448e747b
groot_m <602784238@qq.com>
2017-08-30 21:44:54 +0800

    wealth项目新建远程仓库第二版

---
+++ src/test/java/com/groot/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.groot.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+@Controller
+public class TestController {
+
+
+    public void hello(){
+
+    }
+}
